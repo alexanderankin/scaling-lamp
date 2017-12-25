c6667760e153277cb8d4dfe25d7dbd33974d2af1
Dave Ankin <alexanderankin@gmail.com>
2017-12-25 17:42:19 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var querystring = require('querystring');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var info = {
+    client_id     : 'client_id',
+    redirect_uri  : 'http://localhost:4001/redirect_uri',
+    scope         : '',
+    response_type : '',
+    state         : '',
+  };
+
+  var queryString = querystring.stringify(info);
+  res.render('page', { title: 'Express', page: `
+    <a href="http://localhost:4000/oauth/authorize/?${querystring.stringify(info)}">loginlink</a>
+  ` });
+});
+
+router.get('/redirect_uri', function (req, res, next) {
+  res.render('page', { title: 'Express | redirect_uri', page: `
+    <p>body</p>
+    <pre>${JSON.stringify(req.body)}</pre>
+    <p>query</p>
+    <pre>${JSON.stringify(req.query)}</pre>
+  ` });
+});
+
+module.exports = router;
