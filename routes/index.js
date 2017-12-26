e6e3c44f5bbdbc0c268e186dd04bb50a365ce140
Dave Ankin <alexanderankin@gmail.com>
2017-12-26 16:03:03 -0500

    added multiple scopes, state, token requesting code

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,9 @@ router.get('/', function(req, res, next) {
   var info = {
     client_id     : 'client_id',
     redirect_uri  : 'http://localhost:4001/redirect_uri',
-    scope         : 'mail',
+    scopes        : [ 'mail', 'news' ],
     response_type : 'code',
-    // state         : '',
+    state         : 'keyboard yes',
   };
 
   var queryString = querystring.stringify(info);
@@ -22,12 +22,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/redirect_uri', function (req, res, next) {
-  var code = req.query.code;
+  var grant_type   = "authorization_code";
+  var code         = req.query.code;
+  var redirect_uri = 'http://localhost:4001/redirect_uri';
+
+  var client_id = 'client_id';
+  var client_secret = 'client_secret';
 
   request.post('http://localhost:4000/oauth/token', {
     json: true,
-    body: { code }
+    body: { grant_type, code, redirect_uri, client_id, client_secret }
   }, function (err, resp, body) {
+    if (err) { return res.render('page', { page: `<pre>${JSON.stringify({err})}</pre>`}) }
     var token = body.token;
 
     res.render('page', { title: 'Express | redirect_uri', page: `
